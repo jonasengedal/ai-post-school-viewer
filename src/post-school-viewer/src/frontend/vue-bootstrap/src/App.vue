90e0484c343e4174c0dd2b7bf82dcd381eb320ab
Jonas Nygaard Engedal <jen@cvation.com>
2024-10-05 09:27:28 +0200

    now with working tooltips

--- src/post-school-viewer/src/frontend/vue-bootstrap/src/App.vue
+++ src/post-school-viewer/src/frontend/vue-bootstrap/src/App.vue
@@ -4,17 +4,21 @@ import BSTooltip from '@/components/BSTooltip.vue';
 import BSNavbar from '@/components/BSNavbar.vue';
 
 // Inject Bootstrap with the same key as defined in main.js
-// const bootstrap = inject('bootstrap');
+const bootstrap = inject('bootstrap');
 
-// onMounted(() => {
-//   try {
-//     // Expanding the navbar.
-//     const collapse = new bootstrap.Collapse('#navbarSupportedContent');
-//     collapse.show();
-//   } catch (e) {
-//     console.log('Bootstrap error: ', e);
-//   }
-// });
+onMounted(() => {
+  try {
+    // Expanding the navbar.
+    // const collapse = new bootstrap.Collapse('#navbarSupportedContent');
+    // collapse.show();
+
+    // Tooltips are opt-in and must be initialized manually!
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+  } catch (e) {
+    console.log('Bootstrap error: ', e);
+  }
+});
 </script>
 
 <template>
